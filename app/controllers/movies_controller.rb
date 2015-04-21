fa8adb59c1620e147f9429697cd2eda0194634cc
dianakrasnozhenova <344les48sy92>
2015-04-21 18:45:14 +0300

    Add searchkick and implement search functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,11 +3,26 @@ class MoviesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :verify_owner, only: [:edit, :update, :destroy]
 
+  def search
+    if params[:search].present?
+      @movies = Movie.search(params[:search])
+    else
+      @movies = Movie.all
+    end
+  end
+
   def index
     @movies = Movie.all
   end
 
   def show
+    @reviews = Review.where(movie_id: @movie.id).order("created_at DESC")
+
+    if @reviews.blank?
+      @avg_review = 0
+    else
+      @avg_review = @reviews.average(:rating).round(2)
+    end
   end
 
   def new
